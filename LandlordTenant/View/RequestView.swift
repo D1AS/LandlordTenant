289686dd0e499adb23a097b7fa988fcb604d6e1b
waynewang1888 <wanghj1999@gmail.com>
2025-03-13 09:51:06 -0400

    Add RequestView

---
+++ LandlordTenant/View/RequestView.swift
@@ -0,0 +1,18 @@
+//
+//  RequestView.swift
+//  LandlordTenant
+//
+//  Created by Juan Wang on 2025/3/13.
+//
+
+import SwiftUI
+
+struct RequestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RequestView()
+}
